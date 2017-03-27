524566a20ea8a19daae49d90b824311b073952b4
bin <bin6160@126.com>
2017-03-27 22:04:42 +0800

    refactor net request

--- app/src/main/java/com/dev/bins/bandwagongclient/net/NetWorkManager.java
+++ app/src/main/java/com/dev/bins/bandwagongclient/net/NetWorkManager.java
@@ -8,6 +8,7 @@ import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
+import rx.Observable;
 import rx.Subscriber;
 import rx.Subscription;
 import rx.android.schedulers.AndroidSchedulers;
@@ -42,53 +43,37 @@ public class NetWorkManager {
 
 
     public Subscription getServerInfo(Subscriber<ServerInfo> subscriber, String veid, String api_key) {
-        return mApi.getServerInfo(veid, api_key)
-                .subscribeOn(Schedulers.io())
-                .unsubscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(subscriber);
+        return request(mApi.getServerInfo(veid, api_key),subscriber);
     }
 
     public Subscription getAvailableOS(Subscriber<AvailableOS> subscriber, String veid, String api_key) {
-        return mApi.getAvailableOS(veid, api_key)
-                .subscribeOn(Schedulers.io())
-                .unsubscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(subscriber);
+        return request(mApi.getAvailableOS(veid, api_key),subscriber);
+
     }
 
     public Subscription kill(Subscriber<ResultCode> subscriber, String veid, String api_key) {
-        return mApi.kill(veid, api_key)
-                .subscribeOn(Schedulers.io())
-                .unsubscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(subscriber);
+        return request(mApi.kill(veid, api_key),subscriber);
     }
 
     public Subscription start(Subscriber<ResultCode> subscriber, String veid, String api_key) {
-        return mApi.start(veid, api_key)
-                .subscribeOn(Schedulers.io())
-                .unsubscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(subscriber);
+        return request(mApi.start(veid, api_key),subscriber);
     }
 
     public Subscription stop(Subscriber<ResultCode> subscriber, String veid, String api_key) {
-        return mApi.stop(veid, api_key)
-                .subscribeOn(Schedulers.io())
-                .unsubscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(subscriber);
+        return request(mApi.stop(veid, api_key),subscriber);
     }
 
     public Subscription restart(Subscriber<ResultCode> subscriber, String veid, String api_key) {
-        return mApi.restart(veid, api_key)
-                .subscribeOn(Schedulers.io())
+        return request(mApi.restart(veid, api_key),subscriber);
+    }
+
+
+    public <T> Subscription request(Observable<T> observable, Subscriber<T> subscriber) {
+        return observable.subscribeOn(Schedulers.io())
                 .unsubscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(subscriber);
     }
-
 
 
 }
